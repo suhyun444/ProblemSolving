b1fba182f23ed9fc387ea4cc17a14d3bd02206d7
Suhyun <67677286+suhyun444@users.noreply.github.com>
2023-04-06 12:51:31 +0900

    [Silver II] Title: 나무 자르기, Time: 676 ms, Memory: 8176 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> trees;
+int n;
+long long m;
+bool func(int mid)
+{
+	long long sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += max(trees[i] - mid,0);
+	}
+	return sum >= m;
+}
+int main()
+{
+	int maxHeight = 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		maxHeight = max(maxHeight, a);
+		trees.push_back(a);
+	}
+	int result = 0;
+	int lo = 0;
+	int hi = maxHeight;
+	while (lo + 1 < hi)
+	{
+		int mid = (lo + hi) / 2;
+		if (func(mid))
+		{
+			lo = mid;
+			result = mid;
+		}
+		else
+		{
+			hi = mid;
+		}
+	}
+	cout << result;
+
+}
