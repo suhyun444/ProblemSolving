84cc98702e18da4c07a4d4c189766d48c3960bc4
Suhyun <67677286+suhyun444@users.noreply.github.com>
2023-07-10 16:22:41 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 356 ms, Memory: 362468 KB -BaekjoonHub

---
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int N;
+int cache[22][1 << 22];
+vector<pair<int,int>> work[22];
+int solve(int n,int people)
+{
+	if (n == N)return 0;
+	int& ret = cache[n][people];
+	if (ret != -1)return ret;
+	ret = 2e9;
+	if (work[n].size() == 0)
+	{
+		ret = solve(n + 1, people);
+	}
+	else
+	{
+		for (int i = 0; i < work[n].size(); i++)
+		{
+			if (((people >> work[n][i].first) & 1) == 0)
+			{
+				ret = min(ret, solve(n + 1, people | (1 << work[n][i].first)) + work[n][i].second);
+			}
+		}
+	}
+	return ret;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	memset(cache, -1, sizeof(cache));
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			int d;
+			cin >> d;
+			work[j].push_back({ i,d });
+		}
+	}
+	cout << solve(0, 0);
+}
