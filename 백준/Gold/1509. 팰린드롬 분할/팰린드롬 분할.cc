292962e651a87c35fdbf4e4e7fbd908d84d30fdb
Suhyun <67677286+suhyun444@users.noreply.github.com>
2023-09-06 13:34:24 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 92 ms, Memory: 32644 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -5,6 +5,24 @@ using namespace std;
 
 string s;
 int cache[2501][2501];
+bool isPelindrome[2501][2501];
+void MakePelindromeArray()
+{
+	for (int i = 0; i < s.size() - 1; ++i)
+	{
+		isPelindrome[i][i] = true;
+		if (s[i] == s[i + 1])isPelindrome[i][i + 1] = true;
+	}
+	isPelindrome[s.size() - 1][s.size() - 1] = true;
+	for (int length = 3; length <= s.size(); ++length)
+	{
+		for (int l = 0; l + length - 1 < s.size(); ++l)
+		{
+			int r = l + length - 1;
+			if (s[l] == s[r] && isPelindrome[l + 1][r - 1])isPelindrome[l][r] = true;
+		}
+	}
+}
 int solve(int start, int n,bool canMake)
 {
 	if (n == s.size())
@@ -15,22 +33,8 @@ int solve(int start, int n,bool canMake)
 	int& ret = cache[start][n];
 	if (ret != -1)return ret;
 	ret = 2e9;
-	int l = start, r = n;
-	bool isPelindrome = true;
-	while (l < r)
-	{
-		if (s[l] != s[r])
-		{
-			isPelindrome = false;
-			break;
-		}
-		++l;
-		--r;
-	}
-	if (isPelindrome)
-	{
+	if (isPelindrome[start][n])
 		ret = min(ret, solve(n + 1, n + 1, true) + 1);
-	}
 	ret = min(ret, solve(start, n + 1,false));
 	return ret;
 }
@@ -38,5 +42,6 @@ int main()
 {
 	memset(cache, -1, sizeof(cache));
 	cin >> s;
+	MakePelindromeArray();
 	cout << solve(0, 0,true);
 }
