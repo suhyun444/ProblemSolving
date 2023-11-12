34b86ddaa64d3540a621e5a2146190fdbbaa63ba
Suhyun <67677286+suhyun444@users.noreply.github.com>
2023-11-12 17:06:21 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 2804 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
+using namespace std; 
 
-int city[100001];
-int dist[100001];
-int main() {
+int main()
+{
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 	int n;
 	cin >> n;
-	for (int i = 1; i < n; i++)cin >> dist[i];
-	for (int i = 0; i < n; i++)cin >> city[i];
-	int curCost = city[0];
+	vector<int> dist(n - 1);
+	vector<int> city(n);
+	for (int i = 0; i < n - 1; ++i)
+		cin >> dist[i];
+	for (int i = 0; i < n; ++i)
+		cin >> city[i];
+
+	int minCost = city[0];
 	long long result = 0;
-	for (int i = 1; i < n; i++) {
-		result += 1LL * dist[i] * curCost;
-		if (curCost > city[i]) {
-			curCost = city[i];
-		}
+	for (int i = 0; i < n - 1; ++i)
+	{
+		minCost = min(minCost, city[i]);
+		result += 1LL * minCost * dist[i];
 	}
 	cout << result;
 }
