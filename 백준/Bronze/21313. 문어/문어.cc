347ba9cdac842160a2d08e92fefcc8fde72a857c
Suhyun <67677286+suhyun444@users.noreply.github.com>
2026-01-16 16:08:47 +0900

    [Bronze II] Title: 문어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21313. 문어/문어.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    for(int i=0;i<n/2;++i)
+        cout << "1 2 ";
+    if(n % 2 !=0)
+        cout << "3";
+}
