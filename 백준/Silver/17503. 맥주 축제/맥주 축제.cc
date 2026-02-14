b40da2a4a71406402d1365962a0bdffe5874b6ee
Suhyun <67677286+suhyun444@users.noreply.github.com>
2026-02-14 15:17:29 +0900

    [Silver I] Title: 맥주 축제, Time: 76 ms, Memory: 8296 KB -BaekjoonHub

---
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int n,k;
+long long m;
+long long maxLevel = 0;
+vector<pair<long long,long long>> beer;
+bool compare(pair<int,int> a,pair<int,int> b)
+{
+    return a.first > b.first;
+}
+bool func(int curLevel)
+{
+    int curCount = 0;
+    long long curSatis = 0;
+    for(int i=0;i<k;++i)
+    {
+        if(beer[i].second <= curLevel)
+        {
+            curCount++;
+            curSatis += beer[i].first;
+            if(curCount == n)
+                break;
+        }
+    }
+    return curSatis >= m && curCount == n;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n >> m >> k;
+    for(int i=0;i<k;++i)
+    {
+        long long a,b;
+        cin >> a >> b;
+        maxLevel = max(maxLevel,b);
+        beer.push_back({a,b});
+    }
+    sort(beer.begin(),beer.end(),compare);
+    long long lo = 0;
+    long long hi = maxLevel + 1;
+    int ans = -1;
+    while(lo <= hi)
+    {
+        long long mid = (lo + hi) / 2;
+        if(func(mid))
+        {
+            hi = mid - 1;
+            ans = mid;
+        }
+        else
+        {
+            lo = mid + 1;
+        }
+    }
+    cout << ans;
+}
