60948bcbd73e73f6376f85ae75b7376699502640
Suhyun <67677286+suhyun444@users.noreply.github.com>
2026-02-20 22:36:21 +0900

    [Gold III] Title: 고기잡이, Time: 24 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/7573. 고기잡이/고기잡이.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int n,k;
+int board[201][201];
+int dirY[4] = {0,1,0,-1};
+int dirX[4] = {1,0,-1,0};
+int s,x,y;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n,l,m;
+    cin >> n >> l >> m;
+    vector<pair<int,int>> fish;
+    for(int i=0;i<m;++i)
+    {
+        int x,y;
+        cin >> y >> x;
+        fish.push_back({y,x});
+    }
+    int ans = 0;
+    for(int i=0;i<fish.size();++i)
+    {
+        pair<int,int> cur = fish[i];
+        for(int j=1;j<l/2;++j)
+        {
+            int row = j;
+            int col = l / 2 - row;
+            for(int offset = 0;offset <= col;++offset)
+            {
+                int sy = cur.first;
+                int sx = cur.second - offset;
+                if(sy + row > n + 1 || sx + col > n + 1 || sx < 1 || sy < 1)continue;
+                int count = 0;
+                for(int k=0;k<fish.size();++k)
+                {
+                    pair<int,int> next = fish[k];
+                    if(sy <= next.first && next.first <= sy + row && sx <= next.second && next.second <= sx + col)
+                    {
+                        count++;
+                    }
+                }
+                ans = max(ans,count);
+            }
+        }
+    }
+    cout << ans;
+}
