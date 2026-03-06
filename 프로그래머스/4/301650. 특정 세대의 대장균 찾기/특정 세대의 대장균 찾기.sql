fde412ce03f91002847d653f334f6777235f88e7
Suhyun <67677286+suhyun444@users.noreply.github.com>
2026-03-06 22:50:44 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,1 +1,9 @@
-select c.id as ID from ecoli_data as c inner join ecoli_data as p on c.parent_id = p.id inner join ecoli_data as gp on p.parent_id = gp.id where gp.parent_id is NULL order by c.id;
+with recursive gen as(
+    select id, 1 as generation from ecoli_data where parent_id is null
+    union all
+    select d.id,generation + 1 as generation from gen as r 
+    inner join ecoli_data as d
+    on r.id = d.parent_id
+)
+
+select ID from gen where generation = 3 order by id
