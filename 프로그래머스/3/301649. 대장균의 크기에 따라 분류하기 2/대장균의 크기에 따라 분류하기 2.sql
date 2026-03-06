26a05df3775bfae1884b61bd5dbc8910d4fe5bcb
Suhyun <67677286+suhyun444@users.noreply.github.com>
2026-03-06 22:41:06 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,1 +1,3 @@
-select ID, case ntile(4) over (order by size_of_colony) when 4 then "CRITICAL" when 3 then "HIGH" when 2 then "MEDIUM" when 1 then "LOW" end as COLONY_NAME from ECOLI_DATA order by id;
+select ID, case ntile(4) over(order by size_of_colony) when 1 then "LOW" when 2 then "MEDIUM" when 3 then "HIGH" when 4 then "CRITICAL" end as COLONY_NAME 
+from ecoli_data
+order by id
